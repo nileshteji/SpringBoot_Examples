b543e31d7ab77dab75b1c38897632aa37598a21d
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-05-25 00:30:06 +0530

    Version 1

--- HelloWorldApplication/src/main/java/com/example/HelloWorldApplication/controller/WebClassController.java
+++ HelloWorldApplication/src/main/java/com/example/HelloWorldApplication/controller/WebClassController.java
@@ -1,24 +1,37 @@
 package com.example.HelloWorldApplication.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class WebClassController {
 
-    @GetMapping("/nilesh")
+    @GetMapping("/")
     public String hi(@RequestParam(value = "name",defaultValue = "Nilesh Teji") String name){
-        return "Hello"+" "+name+"!";
-    }
-    @RequestMapping("/")
-    public String welcome(){
-        return "Welcome to Website";
+        return "HelloWorld.html";
     }
 
 
+    @GetMapping("/testing")
+    @ResponseBody
+    public String testing(){
+        return "testing";
+    }
 
 
-}
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", defaultValue="World") String name, Model model) {
+            model.addAttribute("name",name);
+            return "greeting";
+        }
+
+    }
+
+
+
+
+
