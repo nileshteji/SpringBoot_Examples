f4c826a484e0d36ecabf1c2d0f6532b2473c9d9b
Nilesh Teji <kumar.nilesh023@gmail.com>
2020-05-25 02:02:05 +0530

    Added Button click to Controller

--- HelloWorldApplication/src/main/java/com/example/HelloWorldApplication/HelloWorldApplication.java
+++ HelloWorldApplication/src/main/java/com/example/HelloWorldApplication/HelloWorldApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
